49587273c9b92cf83fc30df7d71bafee1a303bc7
Jarrod M. Meyer <meyerja.2013@gmail.com>
2023-09-04 10:45:38 -0400

    Add build.zig

---
+++ build.zig
@@ -0,0 +1,97 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    // No other target is supported right now
+    const target = std.zig.CrossTarget{
+        .cpu_arch = .x86,
+        .os_tag = .windows,
+        .abi = .gnu,
+    };
+
+    const optimize = b.standardOptimizeOption(.{});
+
+    const @"open3dmm-core_dep" = b.dependency("open3dmm-core", .{
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const resinator_dep = b.dependency("resinator", .{
+        .optimize = .ReleaseFast,
+    });
+
+    const exe = b.addExecutable(.{
+        .name = "open3DMM",
+        .target = target,
+        .optimize = optimize,
+        .link_libc = true,
+    });
+    exe.disable_sanitize_c = true;
+
+    exe.linkLibrary(@"open3dmm-core_dep".artifact("open3dmm-core"));
+    exe.installLibraryHeaders(@"open3dmm-core_dep".artifact("open3dmm-core"));
+    exe.addIncludePath(.{ .path = "inc" });
+    exe.addIncludePath(.{ .path = "src/studio" });
+    exe.addCSourceFiles(open3dmm_sources, open3dmm_flags);
+
+    const resinator_exe = resinator_dep.artifact("resinator");
+
+    const resinator_run = b.addRunArtifact(resinator_exe);
+
+    resinator_run.addArgs(&.{
+        "/y",
+        "/i",
+        b.pathFromRoot("inc"),
+        "/i",
+        b.getInstallPath(.header, ""),
+        "/i",
+        b.pathFromRoot("src"),
+    });
+
+    resinator_run.addFileArg(.{ .path = "src/studio/utest.rc" });
+    exe.addObjectFile(resinator_run.addOutputFileArg("utest.res"));
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
+
+const open3dmm_flags: []const []const u8 = &.{
+    "-w",
+    "-DLITTLE_ENDIAN",
+    "-DWIN",
+    "-DSTRICT",
+    "-fms-extensions",
+    "-fno-rtti",
+    "-fno-exceptions",
+};
+
+const open3dmm_sources: []const []const u8 = &.{
+    "src/studio/ape.cpp",
+    "src/studio/browser.cpp",
+    "src/studio/esl.cpp",
+    "src/studio/mminstal.cpp",
+    "src/studio/popup.cpp",
+    "src/studio/portf.cpp",
+    "src/studio/scnsort.cpp",
+    "src/studio/splot.cpp",
+    "src/studio/stdiobrw.cpp",
+    "src/studio/stdioscb.cpp",
+    "src/studio/studio.cpp",
+    "src/studio/tatr.cpp",
+    "src/studio/tgob.cpp",
+    "src/studio/utest.cpp",
+};
